8f34f9c12fcbbf451ec2355e6c77d13f85040c22
bornej <6570429+bornej@users.noreply.github.com>
2021-10-09 15:00:03 +0200

    Syncronized Coordinator methods + wait() while lock released

--- src/main/java/jvn/JvnCoordImpl.java
+++ src/main/java/jvn/JvnCoordImpl.java
@@ -17,11 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.io.Serializable;
 
-
-public class JvnCoordImpl
-        extends UnicastRemoteObject
-        implements JvnRemoteCoord {
-
+public class JvnCoordImpl extends UnicastRemoteObject implements JvnRemoteCoord {
 
     /**
      *
@@ -35,6 +31,7 @@ public class JvnCoordImpl
     private HashMap<String, JvnObject> storeByName = new HashMap<>();
     private HashMap<Integer, JvnObject> storeById = new HashMap<>();
     private HashMap<Integer, List<LockInfo>> storeLocks = new HashMap<>();
+
     /**
      * Default constructor
      *
@@ -51,13 +48,12 @@ public class JvnCoordImpl
     }
 
     /**
-     * Allocate a NEW JVN object id (usually allocated to a
-     * newly created JVN object)
+     * Allocate a NEW JVN object id (usually allocated to a newly created JVN
+     * object)
      *
      * @throws java.rmi.RemoteException,JvnException
      **/
-    public int jvnGetObjectId()
-            throws java.rmi.RemoteException, jvn.JvnException {
+    public int jvnGetObjectId() throws java.rmi.RemoteException, jvn.JvnException {
         return this.lastId++;
     }
 
@@ -72,10 +68,10 @@ public class JvnCoordImpl
     public void jvnRegisterObject(String jon, JvnObject jo, JvnRemoteServer js)
             throws java.rmi.RemoteException, jvn.JvnException {
         // Add the JvnObject to the store if it doesn't already exist
-        if(storeByName.get(jon) == null){
-          storeByName.put(jon, jo);
-          jo.jvnSetObjectId(this.jvnGetObjectId());
-          storeById.put(jo.jvnGetObjectId(), jo);
+        if (storeByName.get(jon) == null) {
+            storeByName.put(jon, jo);
+            jo.jvnSetObjectId(this.jvnGetObjectId());
+            storeById.put(jo.jvnGetObjectId(), jo);
         } else {
             throw new jvn.JvnException("Object " + jon + " is already registered.");
         }
@@ -88,24 +84,23 @@ public class JvnCoordImpl
      * @param js  : the remote reference of the JVNServer
      * @throws java.rmi.RemoteException,JvnException
      **/
-    public JvnObject jvnLookupObject(String jon, JvnRemoteServer js)
-            throws java.rmi.RemoteException, jvn.JvnException {
+    public JvnObject jvnLookupObject(String jon, JvnRemoteServer js) throws java.rmi.RemoteException, jvn.JvnException {
         JvnObject jo = storeByName.get(jon);
 
-        if(jo != null) {
+        if (jo != null) {
             jo = jo.clone();
 
             // Get the locks list for this JvnObject
             List<LockInfo> locks = storeLocks.get(jo.jvnGetObjectId());
-            if(locks == null){
-              // No locks registered for this JvnObject ; add it to the hashmap
-              locks = new ArrayList<LockInfo>();
-              storeLocks.put(jo.jvnGetObjectId(), locks);
+            if (locks == null) {
+                // No locks registered for this JvnObject ; add it to the hashmap
+                locks = new ArrayList<LockInfo>();
+                storeLocks.put(jo.jvnGetObjectId(), locks);
 
-              // Add the lock info to the list
-              locks.add(new LockInfo(js, jo.getLock()));
+                // Add the lock info to the list
+                locks.add(new LockInfo(js, jo.getLock()));
             }
-    
+
         }
 
         return jo;
@@ -119,54 +114,53 @@ public class JvnCoordImpl
      * @return the current JVN object state
      * @throws java.rmi.RemoteException, JvnException
      **/
-    public Serializable jvnLockRead(int joi, JvnRemoteServer js)
+    public synchronized Serializable jvnLockRead(int joi, JvnRemoteServer js)
             throws java.rmi.RemoteException, JvnException {
         List<LockInfo> locks = storeLocks.get(joi);
         // Look for a server with a write lock on the object
         JvnRemoteServer jsWithLock = null;
-        do {
-            for (LockInfo lockInfo : locks) {
-                if(lockInfo.getLock() == Lock.W) {
-                    jsWithLock = lockInfo.getJvnRemoteServer();
-                    break;
-                }
+        Serializable s = null;
+        for (LockInfo lockInfo : locks) {
+            if (lockInfo.getLock() == Lock.W) {
+                jsWithLock = lockInfo.getJvnRemoteServer();
+                break;
             }
+        }
 
-            // Found a server with a lock
-            if(jsWithLock != null){
-                try {
-                    // Wait for it to unlock the object
-                    wait();
-
-                    // Ici je crois qu'il faut mettre la suite du code dans un bloc séparé (si notifyAll alors tous les threads vont se réveiller et invalidate donc pas bon)
-                    // mais pour l'instant balec ; c'est juste pour tester
-                    Serializable s = jsWithLock.jvnInvalidateWriterForReader(joi);
-                    
-                    // Update LockInfo for the server asking for a read lock
-                    Boolean found = null;
-                    for (LockInfo lockInfo : locks) {
-                        if(lockInfo.getJvnRemoteServer() == js){
-                            found = true;
-                            lockInfo.setLock(Lock.R);
-                        }
-                    }
-                    // Si pas de lockInfo dans la liste on en créé un
-                    if(!found){
-                        locks.add(new LockInfo(js, Lock.R));
-                    }
-
-                    // On met à jour l'objet dans le store
-                    // (Il faut aussi le faire dans le storeByName mais pas eu le temps : marche sans pour l'instant à priori)
-                    JvnObject o = storeById.get(joi);
-                    o.jvnSetSharedObject(s);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        try {
+            // If we found a server with a write lock
+            while (jsWithLock != null) {
+                // Ask server for write lock Invalidation
+                s = jsWithLock.jvnInvalidateWriterForReader(joi);
+                // Wait for it to unlock the object and notify us
+                wait();
             }
-        } while(jsWithLock != null);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Update LockInfo for the server asking for a read lock
+        Boolean found = null;
+        for (LockInfo lockInfo : locks) {
+            if (lockInfo.getJvnRemoteServer() == js) {
+                found = true;
+                lockInfo.setLock(Lock.R);
+            }
+        }
+        // Si pas de lockInfo dans la liste on en créé un
+        if (!found) {
+            locks.add(new LockInfo(js, Lock.R));
+        }
+
+        // On met à jour l'objet qui possèdait le lock write dans le store
+        // (Il faut aussi le faire dans le storeByName mais pas eu le temps : marche
+        // sans pour l'instant à priori)
+        JvnObject o = storeById.get(joi);
+        o.jvnSetSharedObject(s);
 
         // Renvoyer l'objet courant
         return storeById.get(joi).jvnGetSharedObject();
+
     }
 
     /**
@@ -177,40 +171,43 @@ public class JvnCoordImpl
      * @return the current JVN object state
      * @throws java.rmi.RemoteException, JvnException
      **/
-    public Serializable jvnLockWrite(int joi, JvnRemoteServer js)
+    public synchronized Serializable jvnLockWrite(int joi, JvnRemoteServer js)
             throws java.rmi.RemoteException, JvnException {
         List<LockInfo> locks = storeLocks.get(joi);
         // Look for a server with a write lock on the object
         JvnRemoteServer jsWithLock = null;
-        do {
-            for (LockInfo lockInfo : locks) {
-                if(lockInfo.getLock() == Lock.W) {
-                    jsWithLock = lockInfo.getJvnRemoteServer();
-                    break;
-                }
-            }
 
-            if(jsWithLock != null){
-                try {
-                    wait();
-                    jsWithLock.jvnInvalidateWriter(joi);
-
-                    // Update LockInfo for the server asking for a read lock
-                    Boolean found = null;
-                    for (LockInfo lockInfo : locks) {
-                        if(lockInfo.getJvnRemoteServer() == js){
-                            found = true;
-                            lockInfo.setLock(Lock.W);
-                        }
-                    }
-                    if(!found){
-                        locks.add(new LockInfo(js, Lock.W));
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        for (LockInfo lockInfo : locks) {
+            if (lockInfo.getLock() == Lock.W) {
+                jsWithLock = lockInfo.getJvnRemoteServer();
+                break;
             }
-        } while(jsWithLock != null);
+        }
+
+        try {
+            // If we found a server with a write lock
+            while (jsWithLock != null) {
+                // Ask the server for write lock invalidation
+                jsWithLock.jvnInvalidateWriter(joi);
+                // Wait for the server to release the lock and notify us.
+                wait();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Update LockInfo for the server asking for the write lock
+        Boolean found = null;
+        for (LockInfo lockInfo : locks) {
+            if (lockInfo.getJvnRemoteServer() == js) {
+                found = true;
+                lockInfo.setLock(Lock.W);
+            }
+        }
+        // If no lockInfo present already create one
+        if (!found) {
+            locks.add(new LockInfo(js, Lock.W));
+        }
 
         return storeById.get(joi).jvnGetSharedObject();
     }
@@ -221,10 +218,7 @@ public class JvnCoordImpl
      * @param js : the remote reference of the server
      * @throws java.rmi.RemoteException, JvnException
      **/
-    public void jvnTerminate(JvnRemoteServer js)
-            throws java.rmi.RemoteException, JvnException {
+    public void jvnTerminate(JvnRemoteServer js) throws java.rmi.RemoteException, JvnException {
         // to be completed
     }
 }
-
- 
