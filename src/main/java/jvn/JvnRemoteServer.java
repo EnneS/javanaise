8b6ef5b317a51af9d860d8cb4f8c298da36bde74
bornej <6570429+bornej@users.noreply.github.com>
2021-10-18 11:02:58 +0200

    JvnRemoteServer: fix javadoc

--- src/main/java/jvn/JvnRemoteServer.java
+++ src/main/java/jvn/JvnRemoteServer.java
@@ -13,7 +13,6 @@ package jvn;
 import java.rmi.*;
 import java.io.*;
 
-
 /**
  * Remote interface of a JVN server (used by a remote JvnCoord)
  */
@@ -24,31 +23,29 @@ public interface JvnRemoteServer extends Remote {
      * Invalidate the Read lock of a JVN object
      *
      * @param joi : the JVN object id
-     * @throws java.rmi.RemoteException,JvnException
+     * @throws java.rmi.RemoteException Remote exception
+     * @throws JvnException             Jvn exception
      **/
-    public void jvnInvalidateReader(int joi)
-            throws java.rmi.RemoteException, jvn.JvnException;
+    public void jvnInvalidateReader(int joi) throws java.rmi.RemoteException, jvn.JvnException;
 
     /**
      * Invalidate the Write lock of a JVN object
      *
      * @param joi : the JVN object id
      * @return the current JVN object state
-     * @throws java.rmi.RemoteException,JvnException
+     * @throws java.rmi.RemoteException Remote exception
+     * @throws JvnException             Jvn exception
      **/
-    public Serializable jvnInvalidateWriter(int joi)
-            throws java.rmi.RemoteException, jvn.JvnException;
+    public Serializable jvnInvalidateWriter(int joi) throws java.rmi.RemoteException, jvn.JvnException;
 
     /**
      * Reduce the Write lock of a JVN object
      *
      * @param joi : the JVN object id
      * @return the current JVN object state
-     * @throws java.rmi.RemoteException,JvnException
+     * @throws java.rmi.RemoteException Remote exception
+     * @throws JvnException             Jvn exception
      **/
-    public Serializable jvnInvalidateWriterForReader(int joi)
-            throws java.rmi.RemoteException, jvn.JvnException;
+    public Serializable jvnInvalidateWriterForReader(int joi) throws java.rmi.RemoteException, jvn.JvnException;
 
 }
-
- 
